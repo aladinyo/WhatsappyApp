4c4f00279a07580d01f823287937f7f4297ed248
Alaa Eddine Boubekeur <49879567+aladinyo@users.noreply.github.com>
2021-04-13 16:59:10 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,16 +5,7 @@ import "firebase/database"
 import "firebase/messaging";
 import "firebase/storage"
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDKpDICrGpe4Afk1j0ffc8dlUG3jWKpgx0",
-    authDomain: "whatsappy-app.firebaseapp.com",
-    databaseURL: "https://whatsappy-app.firebaseio.com",
-    projectId: "whatsappy-app",
-    storageBucket: "whatsappy-app.appspot.com",
-    messagingSenderId: "468435242245",
-    appId: "1:468435242245:web:737f34ce5b750648aa7b6c",
-    measurementId: "G-QL39RMK58H"
-};
+const firebaseConfig = {};/*use your own configuration*/
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
