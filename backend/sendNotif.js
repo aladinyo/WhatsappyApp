9c1e1653adbbfe8e197dec73602c3c91f9493507
Alaa Eddine Boubekeur <49879567+aladinyo@users.noreply.github.com>
2021-04-13 17:13:03 +0300

    Update sendNotif.js

--- backend/sendNotif.js
+++ backend/sendNotif.js
@@ -18,7 +18,7 @@ db.collection("notifications2").add({
     body: "wesh ya na9ch",
     photoURL: "https://lh4.googleusercontent.com/-tbJZXfy1FVg/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucnmWljfyeNPES_8nI3pjSvOLx2BWw/s96-c/photo.jpg",
     href: "https://whatsappy-app.web.app/",
-    token: "ewIn2h6F6WYY-vGVdTaUaI:APA91bHnYUIcHgknxazzeInzA-1ZQpn41G29QkJV8w-PDOszmpxqJdxRGLHcnXxu2jGCLWBLZjXapUY0YTIzI-NbOIwlZNzt1gh82zg4AuRGxOf1oiJeaHgTSJwEZTgHCk4UkKlVneAX",
+    token: "use your token here",
 }).then(() => {
 	console.log("notif added")
 }).catch(e => console.log(e));
